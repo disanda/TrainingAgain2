2c9d1b971eb82acb69e1c06ee85c6d74273f8cf3
disanda <skyanda1@qq.com>
2020-10-21 16:50:02 +0800

    x

--- preModelTest.py
+++ preModelTest.py
@@ -12,21 +12,21 @@ import network.network_1_SSencoder as net2
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-netG1 = net1.Generator(input_dim=256, output_channels = 3, image_size=256, scale=8)# in: [-1,512], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
-pathG1 = '/home/disanda/Desktop/celebaGAN/output/spectral_norm_celeba256_inputDim256_Scale8_4/checkpoints/Epoch_G_(99).pth'
-netG1.load_state_dict(torch.load(pathG1,map_location=device)) #shadow的效果要好一些 
-netD1 = net1.Discriminator_SpectrualNorm(input_dim=256, input_channels = 3, image_size=256, scale=4)# in: [-1,3,1024,1024],out:[], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
-pathD1 = '/home/disanda/Desktop/celebaGAN/output/spectral_norm_celeba256_inputDim256_Scale8_4/checkpoints/Epoch_D_(99).pth'
-netD1.load_state_dict(torch.load(pathD1,map_location=device))
+# netG1 = net1.Generator(input_dim=256, output_channels = 3, image_size=256, scale=8)# in: [-1,512], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
+# pathG1 = '/home/disanda/Desktop/celebaGAN/output/spectral_norm_celeba256_inputDim256_Scale8_4/checkpoints/Epoch_G_(99).pth'
+# netG1.load_state_dict(torch.load(pathG1,map_location=device)) #shadow的效果要好一些 
+# netD1 = net1.Discriminator_SpectrualNorm(input_dim=256, input_channels = 3, image_size=256, scale=4)# in: [-1,3,1024,1024],out:[], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
+# pathD1 = '/home/disanda/Desktop/celebaGAN/output/spectral_norm_celeba256_inputDim256_Scale8_4/checkpoints/Epoch_D_(99).pth'
+# netD1.load_state_dict(torch.load(pathD1,map_location=device))
 
 
-netG2 = net2.Generator_SS()
-netD2 = net2.Discriminator_SS()
+# netG2 = net2.Generator_SS()
+# netD2 = net2.Discriminator_SS()
 
-print(netG1)
-print(dict(netG1.named_parameters()).keys())
-print(netG2)
-print(dict(netG2.named_parameters()).keys())
+# print(netG1)
+# print(dict(netG1.named_parameters()).keys())
+# print(netG2)
+# print(dict(netG2.named_parameters()).keys())
 
 #----------- param load ------------
 # toggle_grad(netG1,False)
@@ -54,3 +54,18 @@ print(dict(netG2.named_parameters()).keys())
 # print(x.shape)
 # z_ = netD2(x)
 # print(z_.shape)
+
+#---------去除指定层---------
+
+#---------torch 1.6 转低版本 ,即关闭zip序列化-----
+netG1 = net1.Generator(input_dim=256, output_channels = 3, image_size=256, scale=8)# in: [-1,512], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
+pathG1 = '/home/disanda/Desktop/celebaGAN/output/spectral_norm_celeba256_inputDim256_Scale8_4/checkpoints/Epoch_G_(99).pth'
+netG1.load_state_dict(torch.load(pathG1,map_location=device)) #shadow的效果要好一些 
+torch.save(netG1.state_dict(),'/home/disanda/Desktop/celebaGAN/output/spectral_norm_celeba256_inputDim256_Scale8_4/G_EP_99.pth',_use_new_zipfile_serialization=False)
+
+netD1 = net1.Discriminator_SpectrualNorm(input_dim=256, input_channels = 3, image_size=256, scale=4)# in: [-1,3,1024,1024],out:[], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
+pathD1 = '/home/disanda/Desktop/celebaGAN/output/spectral_norm_celeba256_inputDim256_Scale8_4/checkpoints/Epoch_D_(99).pth'
+netD1.load_state_dict(torch.load(pathD1,map_location=device))
+torch.save(netD1.state_dict(),'/home/disanda/Desktop/celebaGAN/output/spectral_norm_celeba256_inputDim256_Scale8_4/D_EP_99.pth',_use_new_zipfile_serialization=False)
+
+
